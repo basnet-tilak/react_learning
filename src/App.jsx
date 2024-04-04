3e2d24343a58ea58882e4b7303ddb4cd49fc1a5c
Tilak Basnet <tilak.basnet@outlook.com>
2024-04-04 09:50:32 +0200

    Has been added the  Navigation bar file with implementation of the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import AboutPage from "./pages/AboutPage";
+import AboutPage from './pages/AboutPage';
 import ArticalPage from './pages/ArticalPage';
 import ArticalListPage from './pages/ArticalListPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './NavBar';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element = {<HomePage/>} />
-        <Route path="/about" element = {<AboutPage/>} />
-        <Route path="/artical" element = {<ArticalPage/>} />
-        <Route path="/articallist" element = {<ArticalListPage/>} />
-        <Route path="/notfoundpage" element = {<NotFoundPage/>} />
-      </Routes>
+      <div>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articals" element={<ArticalPage />} />
+          <Route path="/articallist" element={<ArticalListPage />} />
+          <Route path="/notfoundpage" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
